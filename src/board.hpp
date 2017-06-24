db6f3a7210148fe8901f8fc81aff7e7e58dd19c0
Denis Corbin <dar.linux@free.fr>
2017-06-24 16:16:58 +0200

    adding comment on what board class does

--- src/board.hpp
+++ src/board.hpp
@@ -3,6 +3,8 @@
 #include "erreurs.hpp"
 #include "rotation.hpp"
 
+    /// manages a board where pieces can be added to and removed from
+
 class board : public espace<unsigned char>
 {
 public:
@@ -12,8 +14,8 @@ public:
     board & operator = (const board & ref);
 
     bool operator == (const board & ref) const { return espace<unsigned char>::operator ==(ref); };
-    bool add(const vector<cellule> & ou, unsigned char quoi);
-    void remove(const vector<cellule> & ou, unsigned char quoi);
+    bool add(const vector<cellule> & ou, unsigned char quoi);   //< add a piece to the board do nothing and return false if failed
+    void remove(const vector<cellule> & ou, unsigned char quoi); //< remove a piece from the board
     bool find_free_space(signed int & x, signed int & y) const;
 
     bool similaire(const board & ref) const; // retourne true si même board par rotation et/ou retournement (pile/face)
