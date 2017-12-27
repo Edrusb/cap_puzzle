d903787faef50783b9e0e288bfeb5ab3fc633677
Denis Corbin <dar.linux@free.fr>
2017-12-27 19:05:55 +0100

    modified E_BUG definition to be able to be used inside switch()/case structure

--- src/erreurs.hpp
+++ src/erreurs.hpp
@@ -3,7 +3,7 @@
 
 #define E_SYNTAX 1
 #define E_MEM 2
-#define E_BUG { cout << "BUG : ligne " << __LINE__ << endl; throw 3; }
-#define E_IMPOSSIBLE 3
+#define E_BUG if(true) { cout << "BUG : ligne " << __LINE__ << endl; throw 3; }
+#define E_IMPOSSIBLE 4
 
 #endif
